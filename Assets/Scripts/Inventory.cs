7b6ec6e343b8219f4f45e769650eda6e1366ea9f
Brandon Service <brservice@comcast.net>
2021-01-28 16:26:05 -0500

    Adding old scripts

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,275 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Inventory : MonoBehaviour
+{
+    private Inventory[] inventories;
+    public List<PickUp> inventory;
+    public List<string> removedInventory;
+    private PickUp[] items;
+
+    public PickUp badge;
+
+    private float invX;
+    private float invY;
+
+    private int prevCount;
+    private int rowCount;
+
+    public int Count
+    {
+        get { return inventory.Count; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Following code prevents more than one inventory from being created
+        inventories = FindObjectsOfType<Inventory>();
+        if (inventories.Length > 1)
+        {
+            Destroy(inventories[1]);
+            Destroy(inventories[1].gameObject);
+        }
+
+        invX = -1.323f;
+        //invY = -0.788f;
+        invY = -5.0f;
+
+        rowCount = 0;
+
+        items = FindObjectsOfType<PickUp>();
+
+        removedInventory = new List<string>();
+
+        //Allows the inventory to be accessed in other scenes
+        DontDestroyOnLoad(this);
+
+        if(badge != null)
+        DontDestroyOnLoad(badge);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Checking to see if the scene is the main menu, if so, clear reset inventory
+        if(SceneManager.GetActiveScene().name == "MainMenu" || SceneManager.GetActiveScene().name == "Credits")
+        {
+            ResetInventory();
+        }
+
+        items = FindObjectsOfType<PickUp>();
+        CheckForDuplicates();
+        if (inventory.Count > 0)
+        {            
+            if (prevCount != inventory.Count)
+            {
+                if(inventory.Count > 8)
+                {
+                    //If the inventory goes to the next row, run two loops for each row
+                    for (int i = 0; i < 8; i++)
+                    {
+                        DisplayItem(inventory[i], invX, invY, i);
+                    }
+                    for (int i = 8; i < inventory.Count; i++)
+                    {
+                        //Set new x and y for the lower row
+                        float x = -1.689f;
+                        //float y = -1.146f;
+                        float y = -1.346f;
+                        //Rowcount serves as "i"
+                        //Only used because i does not start at 0
+                        rowCount++;
+                        DisplayItem(inventory[i], x, y, rowCount);
+                    }
+                    //Reset row count
+                    rowCount = 0;
+                }
+                else
+                {
+                    //if there is only one row, display as normal
+                    invX = -1.323f;
+                    //invY = -0.788f;
+                    invY = -0.988f;
+                    for (int i = 0; i < inventory.Count; i++)
+                    {
+                        DisplayItem(inventory[i], invX, invY, i);
+                    }
+                }
+                SaveList();
+            }
+        }
+        prevCount = inventory.Count;
+    }
+
+    //Add item to list
+    public void AddItem(PickUp item)
+    {
+        inventory.Add(item);
+    }
+
+    //Remove item from list
+    public void  RemoveItem(PickUp item)
+    {
+        for (int i = 0; i < inventory.Count; i++)
+        {
+            if(inventory[i].Name == item.Name)
+            {
+                inventory[i].Added = false;
+                removedInventory.Add(inventory[i].Name);
+                inventory.RemoveAt(i);
+            }
+        }       
+    }    
+
+    public void DisplayItem(PickUp item, float x, float y, int index)
+    {
+        //Display at the x value according to the index of where it is in the list
+        x += 0.366f * (float)index;
+
+        //Display the item
+        item.transform.position = new Vector2(x, y);
+    }
+
+    //Save items in the inventory so they traverse the scenes
+    public void SaveList()
+    {
+        if (inventory.Count > 0)
+        {
+            for (int i = 0; i < inventory.Count; i++)
+            {
+                DontDestroyOnLoad(inventory[i]);
+            }
+        }
+    }
+
+    //Check if there are duplicate items
+    public void CheckForDuplicates()
+    {
+        //Loop through all items on the scene
+        for (int i = 0; i < items.Length; i++)
+        {
+            //Continue if item is not added
+            if (!items[i].Added)
+            {
+                //Check all of the items in the inventory
+                for (int j = 0; j < inventory.Count; j++)
+                {
+                    //If the item is not added and is in the inventory, it is a duplicate
+                    if (inventory[j].Name == items[i].Name)
+                    {
+                        //Destroy duplicate
+                        Destroy(items[i].gameObject);
+                    }
+                }
+                //If the item is not added and the removed inventory list has at least one thing inside of it, continue check
+                if(removedInventory.Count > 0)
+                {
+                    //Check all of the items in the removed items inventory
+                    for (int j = 0; j < removedInventory.Count; j++)
+                    {
+                        //If the item is not added and is in the removed inventory, it is a duplicate
+                        if (removedInventory[j] == items[i].Name)
+                        {
+                            //Destroy duplicate
+                            Destroy(items[i].gameObject);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    //Returns an item in the inventory if the player has it
+    public PickUp GetItemByName(string name)
+    {
+        for(int i = 0; i < inventory.Count; i++)
+        {
+            if(name == inventory[i].Name)
+            {
+                return inventory[i];
+            }
+        }
+
+        return null;
+    }
+
+    //Returns true or false depending on if you have an item or not
+    public bool HaveItem(string name)
+    {
+        for (int i = 0; i < inventory.Count; i++)
+        {
+            if (name == inventory[i].Name)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    //To combine items
+    void CombineItems(PickUp obj1, PickUp obj2, PickUp newObj)
+    {
+
+        RemoveItem(obj1);
+        RemoveItem(obj2);
+        AddItem(newObj);
+    }
+
+    //Reset method
+    public void ResetInventory()
+    {
+        for (int i = 0; i < inventory.Count; i++)
+        {
+            inventory[i].Added = false;
+            Destroy(inventory[i]);
+            Destroy(inventory[i].gameObject);
+        }
+
+        removedInventory.Clear();
+        inventory.Clear();
+    }
+
+    public void AddBadge()
+    {
+        badge.Added = true;
+        AddItem(badge);
+        if (inventory.Count > 8)
+        {
+            //If the inventory goes to the next row, run two loops for each row
+            for (int i = 0; i < 8; i++)
+            {
+                DisplayItem(inventory[i], invX, invY, i);
+            }
+            for (int i = 8; i < inventory.Count; i++)
+            {
+                //Set new x and y for the lower row
+                float x = -1.689f;
+                //float y = -1.146f;
+                float y = -1.346f;
+                //Rowcount serves as "i"
+                //Only used because i does not start at 0
+                rowCount++;
+                DisplayItem(inventory[i], x, y, rowCount);
+            }
+            //Reset row count
+            rowCount = 0;
+        }
+        else
+        {
+            //if there is only one row, display as normal
+            invX = -1.689f;
+            //invY = -0.788f;
+            invY = -0.988f;
+            for (int i = 0; i < inventory.Count; i++)
+            {
+                DisplayItem(inventory[i], invX, invY, i);
+            }
+        }
+        SaveList();
+    }
+}
+
---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,384 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public string name;
+    private Inventory inventory;
+    public InteractingObjects interObjRef;
+    public GameManager gManager;
+
+    public bool frozen = false;
+    public bool talking = false;
+
+    Vector2 cursorPosition;
+
+    private bool added;
+
+    private PauseTest pauseMenu;
+
+    private AudioSource soundEffect;
+
+    // Cursor Controls
+    public Texture2D specialTexture;
+    public Texture2D normalTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
+    // Dialogue for the item once it's in the inventory.
+    public Dialogue invDialogue;
+
+    public string Name
+    {
+        get { return name; }
+    }
+
+    public bool Added
+    {
+        get { return added; }
+        set
+        {
+            added = value;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {        
+        inventory = FindObjectOfType<Inventory>();
+        pauseMenu = FindObjectOfType<PauseTest>();
+        added = false;
+        soundEffect = GetComponent<AudioSource>();
+
+        if (this.tag == "Ball" || this.tag == "Screwdriver") { frozen = true; }
+
+        gManager = FindObjectOfType<GameManager>();
+
+        if(pauseMenu == null)
+        {
+            pauseMenu = new PauseTest();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!pauseMenu.Paused)
+        {
+            CheckForClick();
+            if (this.tag == "Badge") { UseItem("Badge", "t_badge"); }
+
+            if (this.tag == "Stick") { UseItem(this.tag, "t_Tree"); }
+
+            if (this.tag == "Wrench") { UseItem(this.tag, "t_MechanicFairy"); }
+
+            if (this.tag == "Mitten") { UseItem(this.tag, "t_GreenFairy"); }
+
+            if (this.tag == "Wrench") { UseItem(this.tag, "t_MechanicFairy"); }
+
+            if (this.tag == "Scissors") { UseItem(this.tag, "t_Web"); }
+
+            if (this.tag == "Mitten") { UseItem(this.tag, "t_GreenFairy"); }
+
+            if (this.tag == "Ball" || this.tag == "Screwdriver")
+            {
+                if(!this.frozen && this.transform.position.y > -0.25)
+                {
+                    Vector3 newY = this.transform.position;
+                    newY.y -= 0.01f;
+                    this.transform.position = newY;
+                }
+
+                UseItem("Ball", "Target"); 
+                UseItem("Screwdriver", "Target"); 
+            }
+        }
+
+        if (inventory.HaveItem("Cheese"))
+        {
+            gManager.hasCheese = true;
+        }
+        if (inventory.HaveItem("Pizzabox"))
+        {
+            gManager.hasPizzaBox = true;
+        }
+        if (inventory.HaveItem("Tomato"))
+        {
+            gManager.hasTomatoe = true;
+        }
+        if (inventory.HaveItem("Pizzabox"))
+        {
+            gManager.hasPizzaBox = true;
+        }
+        if (inventory.HaveItem("Water Sludge"))
+        {
+            gManager.hasWater = true;
+        }
+        Debug.Log(gManager.hasWater);
+    }
+
+    //Method to check for click
+    public bool CheckForClick()
+    {
+        if (!pauseMenu.Paused)
+        {
+            //Grab vector2 for cursor to use in AABB math
+            cursorPosition = Input.mousePosition;
+            cursorPosition = Camera.main.ScreenToWorldPoint(cursorPosition);
+
+            //Selection for objects
+            if (Input.GetMouseButtonDown(0) && !this.frozen && !this.talking)
+            {
+                //AABB collision test for cursor
+                if (cursorPosition.x < this.GetComponent<BoxCollider2D>().bounds.max.x && cursorPosition.x > this.GetComponent<BoxCollider2D>().bounds.min.x)
+                {
+                    //Potential collision!
+                    //Check the next condition in a nested if statement, just to not have a ton of &'s and to be more efficient
+                    if (cursorPosition.y > this.GetComponent<BoxCollider2D>().bounds.min.y && cursorPosition.y < this.GetComponent<BoxCollider2D>().bounds.max.y)
+                    {
+                        //Collision!
+
+                        //If the item is clicked on again after it's been added to the inventory, remove it
+                        if (added)
+                        {
+                            //transform.position = new Vector2(100.0f, 100.0f);  Commenting this out so that players don't accidentally delete their items.
+                            if (interObjRef != null)
+                                interObjRef.Temp = this;
+                            //inventory.RemoveItem(this);  Commenting this out so that players don't accidentally delete their items.
+                            // Adding fun dialogue instead!
+                            FindObjectOfType<DialogueManager>().StartDialogue(invDialogue);
+                        }
+                        else
+                        {
+                            //Checking to make sure the inventory isn't full
+                            if (inventory.Count < 16)
+                            {
+                                //Add to inventory
+                                inventory.AddItem(this);
+                                added = true;
+                                soundEffect.PlayOneShot(soundEffect.clip);
+                            }
+                        }
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        return false;
+    }
+
+    // Triggers dialogue about the pickup.
+    public void OnMouseDown()
+    {
+        if (!pauseMenu.Paused)
+        {
+            if (!added)
+            {
+                //GetComponent<SpriteRenderer>().enabled = false;
+                GetComponent<DialogueTrigger>().TriggerDialogue();
+            }
+        }
+    }
+
+    //To pull an item from your inventory to use or of the ground
+    public void UseItem(Vector2 target, string itemTag)
+    {
+        if (!inventory.HaveItem(this.name))
+        {
+            //Debug.Log("HIT return");
+            //return;
+        }
+
+        //To store the mouses position
+        Vector2 locationOfMouse = Input.mousePosition;
+        //Grab vector2 for cursor to use in AABB math
+        cursorPosition = Input.mousePosition;
+        cursorPosition = Camera.main.ScreenToWorldPoint(cursorPosition);
+
+        //Selection for objects
+        if (Input.GetMouseButton(1) && !this.frozen)
+        {
+            //AABB collision test for cursor
+            if (cursorPosition.x < this.GetComponent<BoxCollider2D>().bounds.max.x && cursorPosition.x > this.GetComponent<BoxCollider2D>().bounds.min.x)
+            {
+                //Potential collision!
+                //Check the next condition in a nested if statement, just to not have a ton of &'s and to be more efficient
+                if (cursorPosition.y > this.GetComponent<BoxCollider2D>().bounds.min.y && cursorPosition.y < this.GetComponent<BoxCollider2D>().bounds.max.y)
+                {
+                    //Collision!
+                    this.transform.position = cursorPosition;
+
+                    //to see if the item is near the target zone
+                    if (Vector2.Distance(this.transform.position, target) <= 0.2f)
+                    {
+                        //Validating item
+                        if (this.tag != itemTag)
+                        {
+                            Debug.Log("HIT RETURN: " + this.tag + " " + itemTag);
+                            return;
+                        }
+                        //Do something with the target zone and/or the object in use
+                        HitZone(itemTag);
+                    }
+                }
+            }
+        }
+    }
+
+    //To pull an item from your inventory to use or of the ground using tags
+    public void UseItem(string itemTag, string targetTag)
+    {
+        if (!inventory.HaveItem(this.name))
+        {
+            //Debug.Log("HIT return");
+            //return;
+            //pos = this.transform.position;
+        }
+
+        //To store the mouses position
+        Vector2 locationOfMouse = Input.mousePosition;
+        //Grab vector2 for cursor to use in AABB math
+        cursorPosition = Input.mousePosition;
+        cursorPosition = Camera.main.ScreenToWorldPoint(cursorPosition);
+
+        //To get an item with the desired tag
+        GameObject taggedItem;
+        taggedItem = GameObject.FindWithTag(targetTag);
+
+        //Selection for objects
+        if (Input.GetMouseButton(1) && !this.frozen && !this.talking)
+        {
+            //AABB collision test for cursor
+            if (cursorPosition.x < this.GetComponent<BoxCollider2D>().bounds.max.x && cursorPosition.x > this.GetComponent<BoxCollider2D>().bounds.min.x)
+            {
+                //Potential collision!
+                //Check the next condition in a nested if statement, just to not have a ton of &'s and to be more efficient
+                if (cursorPosition.y > this.GetComponent<BoxCollider2D>().bounds.min.y && cursorPosition.y < this.GetComponent<BoxCollider2D>().bounds.max.y)
+                {
+                    //Collision!
+                    this.transform.position = cursorPosition;
+                    
+                    //to see if the item is near the target zone
+                    if (Vector2.Distance(this.transform.position, taggedItem.transform.position) <= 0.2f)
+                    {
+                        //Validating item
+                        if (this.tag != itemTag)
+                        {
+                            Debug.Log("HIT RETURN: " + this.tag + " " + itemTag);
+                            return;
+                        }
+                        //Do something with the target zone and/or the object in use
+                        HitZone(itemTag);
+                        if (name == "Badge")
+                        {
+                            inventory.AddBadge();
+                        }
+                        added = false;
+                        inventory.RemoveItem(this);
+                    }
+                }
+            }
+        }
+    }
+
+    public void HitZone(string item)
+    {
+        switch (item)
+        {
+            case "Cheese":
+                break;
+            case "Baseball":
+                break;
+            case "RC":
+                break;
+            case "Apple":
+                break;
+            case "Stick":
+                //Ball interaction
+                PickUp[] items = FindObjectsOfType<PickUp>();
+                for(int i = 0; i < items.Length; i++)
+                {
+                    if(items[i].tag == "Ball") { items[i].frozen = false; }
+                }
+                break;
+            case "Wrench":
+                //Mechanic Fairy interaction
+                //Drops screwdriver
+                PickUp[] itemss = FindObjectsOfType<PickUp>();
+                for (int i = 0; i < itemss.Length; i++)
+                {
+                    if (itemss[i].tag == "Screwdriver") { itemss[i].frozen = false; }
+                }
+                gManager.conditionalBools[15] = true;
+                break;
+            case "Glasses":
+                break;
+            case "Flashlight":
+                break;
+            case "Badge":
+                //Employee Dialogue
+                gManager.conditionalBools[10] = true;
+                break;
+            case "HouseKey":
+                break;
+            case "Scissors":
+                //Cuts scissors
+
+                break;
+            case "Mitten":
+                //Orange Fairy interaction
+                gManager.conditionalBools[13] = true;
+                Debug.Log("HIT");
+
+                break;
+            case "Pizza":
+                break;
+            case "IDcard":
+                break;
+            case "Pencil":
+                break;
+            case "Battery":
+                break;
+            case "Rock":
+                break;
+            case "Newspaper":
+                break;
+            case "Key":
+                break;
+            case "Cookie":
+                break;
+            case "Soda":
+                break;
+            case "Crowbar":
+                break;
+            case "Papers":
+                break;
+            case "Screwdriver":
+                //FFFFFFFFFFFFFFFFFF
+
+                break;
+            case "Quarters":
+                break;
+            case "Ball":
+                break;
+            case "PizzaBox":
+                break;
+            default:
+                break;
+
+        }
+    }
+
+    // Cursor changers
+    void OnMouseEnter()
+    {
+        Cursor.SetCursor(specialTexture, hotSpot, cursorMode);
+    }
+
+    void OnMouseExit()
+    {
+        Cursor.SetCursor(normalTexture, hotSpot, cursorMode);
+    }
+}
