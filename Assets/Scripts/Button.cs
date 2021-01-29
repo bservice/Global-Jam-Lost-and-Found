353d85f61922921dabb2447ee523e2b14a1c9ce5
Brandon Service <brservice@comcast.net>
2021-01-29 15:06:56 -0500

    Adding pause functionality and menus

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+    private bool clicked;
+
+    public string sceneName;
+
+    public bool Clicked
+    {
+        get { return clicked; }
+        set
+        {
+            clicked = value;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        clicked = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnMouseDown()
+    {        
+        gameObject.GetComponent<SpriteRenderer>().color = new Color(210f / 255f, 198f / 255f, 140f / 255f);
+        clicked = true;
+        SceneManager.LoadScene(sceneName);
+    }
+}
