1d8d00f716a34df0636c628bad4d15aa48eaa4e8
alessandro <anf9790@g.rit.edu>
2021-01-30 16:26:13 -0500

    FIXED THE TEXT

--- Assets/Scripts/ScoreHolder.cs
+++ Assets/Scripts/ScoreHolder.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // Need to add this for "Text" class.
+using UnityEngine.SceneManagement; // Need to add this to check the scene name.
 
 public class ScoreHolder : MonoBehaviour
 {
     private int finalScore;
+    private Text textScore;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +18,11 @@ public class ScoreHolder : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        textScore = FindObjectOfType<Text>();
+        if (SceneManager.GetActiveScene().name == "GameOver")
+        {
+            textScore.text = "" + finalScore;
+        }
     }
 
     public void GetScore(int score)
